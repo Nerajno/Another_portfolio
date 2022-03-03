b544f06820a0860edd4cceea099b101d6cec515c
nerajno <nerajno@gmail.com>
2022-03-02 20:52:55 -0500

    added another blog

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Footer(){
     return(
         <footer>
-            <div className='container'>
+            <div className='container footer'>
                 <p> Footer text</p>
             </div>
         </footer>
